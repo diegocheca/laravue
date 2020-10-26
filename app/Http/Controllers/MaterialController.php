7a1492b0a3a85d3484cec64e18ecd9bb999d8ce3
diegocheca <diegochecarelli@gmail.com>
2020-10-26 23:16:57 +0100

    materiales primer paso
    materiales primer paso

---
+++ app/Http/Controllers/MaterialController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Laravue\Models\Material;
+use Illuminate\Http\Request;
+use App\Http\Resources\MaterialResource;
+
+
+
+class MaterialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return MaterialResource::collection(Material::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Laravue\Models\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Material $material)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Laravue\Models\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Material $material)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Laravue\Models\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Material $material)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Laravue\Models\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Material $material)
+    {
+        //
+    }
+}
